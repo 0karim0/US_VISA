5565fac9f1f767dc1420be3d0009e44878e22c5f
karim hazem <karimhazem321@gmail.com>
2025-04-06 14:29:47 +0200

    add all folders

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Karim Hazem",
+    author_email="karimhazem321@gmail.com",
+    packages=find_packages()
+)
